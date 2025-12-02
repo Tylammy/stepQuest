05fd091933dc0a9d96061f7e58686c1fe63f07a2
Christopher Nguyen <christophertn0099@gmail.com>
2025-12-02 00:38:29 -0500

    Connect app to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StepQuestApp());
 }
+
 
 class StepQuestApp extends StatelessWidget {
   const StepQuestApp({super.key});
